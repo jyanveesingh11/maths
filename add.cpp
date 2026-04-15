397bbca5ab31ebe2543c204449d6e172019e9379
jyanvee <jyanveesingh1104@gmail.com>
2026-04-15 09:47:15 +0530

    i made a file of maths

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter value of a : ";
+    cin>>a;
+    cout<<"Enter value of b : ";
+    cin>>b;
+    c = a+b;
+    cout<<"sum of a and b="<<c;2
+}
